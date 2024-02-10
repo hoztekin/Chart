621d983083b04a2f17fa94274787c43045860172
Halil OZTEKIN <hoztekin81@hotmail.com>
2024-02-10 16:20:36 +0300

    -Update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,7 +19,18 @@ namespace Chart.Controllers
 
 		public IActionResult Index()
 		{
-            List<DataPoint> dataPoints = new List<DataPoint>();
+            
+			return View();
+        }
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		public IActionResult Dougnut()
+		{
+			List<DataPoint> dataPoints = new List<DataPoint>();
 
 			//dataPoints.Add(new DataPoint("Herbal Medicines", 41));
 			//dataPoints.Add(new DataPoint("Aroma Therapy", 22));
@@ -32,18 +43,42 @@ namespace Chart.Controllers
 
 			var result = db.Examples.ToList();
 
-			foreach (var item in result) 
+			foreach (var item in result)
 			{
 				dataPoints.Add(new DataPoint(item.Quarter, item.Sales));
 			}
 
-            ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
+			ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
+			return View();
+		}
 
-            return View();
-        }
-
-		public IActionResult Privacy()
+		public IActionResult BarChart()
 		{
+			List<DataPoint> dataPointsbar = new List<DataPoint>();
+
+			dataPointsbar.Add(new DataPoint("Albert", 10));
+			dataPointsbar.Add(new DataPoint("Tim", 30));
+			dataPointsbar.Add(new DataPoint("Wilson", 17));
+			dataPointsbar.Add(new DataPoint("Joseph", 39));
+			dataPointsbar.Add(new DataPoint("Robert", 30));
+			dataPointsbar.Add(new DataPoint("Sophia", 25));
+			dataPointsbar.Add(new DataPoint("Emma", 15));
+
+			ViewBag.DataPointsBar = JsonConvert.SerializeObject(dataPointsbar);
+			return View();
+		}
+
+		public IActionResult FunnelChart() 
+		{
+			List<DataPoint> dataPoints = new List<DataPoint>();
+
+			dataPoints.Add(new DataPoint("Total", 100));
+			dataPoints.Add(new DataPoint("Verified", 80));
+			dataPoints.Add(new DataPoint("Processed", 75));
+			dataPoints.Add(new DataPoint("Shipped", 70));
+			dataPoints.Add(new DataPoint("Delivered", 68));
+
+			ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
 			return View();
 		}
 
